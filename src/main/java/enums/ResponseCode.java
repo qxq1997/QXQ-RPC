312775d7b4ae7e71e11bab9662e67cfb2162f08f
qxq <962850601@qq.com>
2021-03-01 22:49:13 +0800

    修改包名

---
+++ src/main/java/enums/ResponseCode.java
@@ -0,0 +1,36 @@
+package enums;
+
+/**
+ * @author by QXQ
+ * @date 2021/2/27.
+ */
+public enum ResponseCode {
+
+    SUCCESS(0,"成功"),
+    FAIL(1,"失败"),
+    ;
+
+    private int code;
+    private String message;
+
+    ResponseCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
